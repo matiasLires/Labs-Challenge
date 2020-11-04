e168ade3b6adc6abb88e01630eb8c56bf40bf753
dgtech369 <lires.matiash@gmail.com>
2020-11-04 16:21:36 -0300

    first-update

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="header">
+        <a href="/">Mercado Challenge</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
